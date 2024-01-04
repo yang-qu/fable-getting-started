037f4dfed3b3b2ba12e6095770a90f64e6151b34
Yang Qu <52755+yang-qu@users.noreply.github.com>
2024-01-04 20:32:24 +1000

    Random increase/decrease and increase delayed

--- src/App.fs
+++ src/App.fs
@@ -8,12 +8,29 @@ let countViewer = document.getElementById "countViewer"
 
 let mutable currentCount = 0
 
+let runAfter (ms: int) callback =
+    async {
+        do! Async.Sleep ms
+        do callback() 
+    }
+    |> Async.StartImmediate
+
+let increaseDelayed = document.getElementById "increaseDelayed"
+   
+let rnd = System.Random()
+
+increaseDelayed.onclick <- fun _ ->
+    runAfter 1000 (fun () ->
+        currentCount <- currentCount + rnd.Next(5, 10)
+        countViewer.innerText <- $"Count is at {currentCount}"
+        )
+ 
 increase.onclick <- fun ev ->
-    currentCount <- currentCount + 1
+    currentCount <- currentCount + rnd.Next(5, 10)
     countViewer.innerText <- $"Count is at {currentCount}"
     
 decrease.onclick <- fun ev ->
-    currentCount <- currentCount + 1
+    currentCount <- currentCount + rnd.Next(5, 10)
     countViewer.innerText <- $"Count is at {currentCount}"
     
 countViewer.innerText <- $"Count is at {currentCount}"
